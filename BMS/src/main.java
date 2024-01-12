3fe49cdd55bc5d96c66b5233f7b81b40a9ede0cc
thuhoangle <thisishoangf@gmail.com>
2024-01-12 08:40:22 +0700

    LOGIN AND HOMEPAGE ADDED

---
+++ BMS/src/main.java
@@ -0,0 +1,19 @@
+
+
+
+/**
+ *
+ * @author thuhoang
+ */
+
+public class main {
+    
+    public static void main(String[] args) {
+        loginUI frame = new loginUI();
+        frame.setVisible(true); // make it appear
+        frame.pack();   // resize the frame to its
+        frame.setLocationRelativeTo(null);  // appear center the screen
+
+    }
+    
+}
